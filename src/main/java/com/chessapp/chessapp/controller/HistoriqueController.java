af0de19243b73ba6ae5c2ebac8f321af9618f808
Chicktife <matteobelz@gmail.com>
2024-06-07 12:20:03 +0200

    Ecriture Historique + create name Rappel : Le path de Player n'est pas bon Data au lieu de Data/Joueurs

--- src/main/java/com/chessapp/chessapp/controller/HistoriqueController.java
+++ src/main/java/com/chessapp/chessapp/controller/HistoriqueController.java
@@ -10,14 +10,7 @@ import java.time.format.DateTimeFormatter;
 import com.chessapp.chessapp.model.Tuple;
 
 public class HistoriqueController {
-
-    public static void ecritureHistorique(String pseudo1, String pseudo2, Tuple tuple1, Tuple tuple2) throws IOException {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm");
-        String formattedDate = now.format(formatter);
-
-        String fileName = pseudo1 + "-" + pseudo2 + "-" + formattedDate + ".csv";
-
+    public static void ecritureHistorique(String fileName, Tuple tuple1, Tuple tuple2) throws IOException {
         String directoryPath = "Data/Historique";
         File directory = new File(directoryPath);
         if (!directory.exists()) {
@@ -34,18 +27,28 @@ public class HistoriqueController {
             writer.write(tuple1.getFirst() + "," + tuple1.getSecond() + "," + tuple2.getFirst() + "," + tuple2.getSecond() + "\n");
         } catch (IOException e) {
             e.printStackTrace();
-            throw new IOException("Problem occurred while writing to the file");
+            throw new IOException("Problème durant l'écriture du fichier");
         }
     }
 
-    public static void lectureHistorique(String file) throws IOException {
-        // lecture
-        // move(co1, co2)
+    public static String crateName(String pseudo1, String pseudo2) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH:mm");
+        String formattedDate = now.format(formatter);
+
+        return pseudo1 + "-" + pseudo2 + "-" + formattedDate + ".csv";
     }
 
+    public static void lectureHistorique(String file) throws IOException {
+        // Implement reading functionality if needed
+    }
+
+
     public static void main(String[] args) {
+
         String pseudo1 = "Player1";
         String pseudo2 = "Player2";
+        String fileName = crateName(pseudo1, pseudo2);
 
         // Simulate a few moves
         Tuple move1 = new Tuple(1, 2);
@@ -59,8 +62,8 @@ public class HistoriqueController {
 
         try {
             // Writing the moves to the history file
-            controller.ecritureHistorique(pseudo1, pseudo2, move1, move2);
-            controller.ecritureHistorique(pseudo1, pseudo2, move3, move4);
+            controller.ecritureHistorique(fileName, move1, move2);
+            controller.ecritureHistorique(fileName, move3, move4);
         } catch (IOException e) {
             e.printStackTrace();
         }
